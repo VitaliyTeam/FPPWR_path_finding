bec37ae86cbf9421f6ce851e7332f5cfd0834aa7
Merden_Hran <merdenhran@gmail.com>
2021-09-27 22:32:33 +0300

    Add Graphic object and some notes to Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -12,14 +12,20 @@ namespace FPPWR_path_finding
 {
     public partial class Form1 : Form
     {
+        Graphic graphic;
+        Graphics graphics;
+        // Calculator не оглошувати! Він static 
+
         public Form1()
         {
             InitializeComponent();
+            graphics = pictureBox.CreateGraphics();
+            graphic = new Graphic(graphics);
         }
 
         private void btnCreateNewMap_Click(object sender, EventArgs e)
         {
-
+            //allPoints = Calculator.generatePoints(pointsCount, pictBoxArea.Width, pictBoxArea.Height);
         }
 
         private void btnDrawNet_Click(object sender, EventArgs e)
